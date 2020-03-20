884cc1ce0a562e988e4c4e33cd836cec98afdfb4
Siyu <jiansiyu@gmail.com>
2020-03-20 00:40:50 -0400

    add scripts to detect difference

---
+++ Scripts/checkIdentical.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+Check_Folder1 = "/home/newdriver/Storage/Research/CRex_Experiment/CRex_Database/DB_CREX/"
+Check_Folder2 = "/home/newdriver/Storage/Research/CRex_Experiment/RasterReplay/DB/"
+
+#run first step, get list of file in the first folder, and check existance of file in the second folder
+
+List 
